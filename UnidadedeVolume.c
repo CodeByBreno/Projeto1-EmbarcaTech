9598e2478a4bd6174078c706b2dadb139e68c299
Lucas Cotrim <quinhacotrim@gmail.com>
2024-12-21 14:35:44 -0300

    Conversão de Mililitros para outros volumes

--- UnidadedeVolume.c
+++ UnidadedeVolume.c
@@ -4,3 +4,8 @@ void litrosParaOutros(double litros) {
     printf("- %.2f mililitros\n", litros * 1000);
     printf("- %.6f metros cúbicos\n", litros / 1000);
 }
+void mililitrosParaOutros(double mililitros) {
+    printf("\n%.2f mililitros equivalem a:\n", mililitros);
+    printf("- %.2f litros\n", mililitros / 1000);
+    printf("- %.9f metros cúbicos\n", mililitros / 1000000);
+}
