248f7a9412e0293f21cb9b0a2e857468b521e953
Lucas Cotrim <quinhacotrim@gmail.com>
2024-12-21 14:19:59 -0300

    Adicionado menu interativo com opções de conversão

--- UnidadedeVolume.c
+++ UnidadedeVolume.c
@@ -4,3 +4,19 @@ int main() {
     printf("Conversor de Valores de Volume\n");
     return 0;
 }
+int main() {
+    int opcao;
+
+    printf("Conversor de Valores de Volume\n");
+    printf("Escolha uma opcao:\n");
+    printf("1 - Litros para Mililitros e Metros Cúbicos\n");
+    printf("2 - Mililitros para Litros e Metros Cúbicos\n");
+    printf("3 - Metros Cúbicos para Litros e Mililitros\n");
+    printf("0 - Sair\n");
+
+    printf("\nDigite sua opcao: ");
+    scanf("%d", &opcao);
+
+    printf("Opcao escolhida: %d\n", opcao);
+    return 0;
+}
