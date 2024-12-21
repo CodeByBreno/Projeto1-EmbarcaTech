15d5a2633f106feb5fd7290b4e27fe50fe9cfcef
Lucas Cotrim <quinhacotrim@gmail.com>
2024-12-21 14:32:32 -0300

    Conversão de litros para outros volumes

--- UnidadedeVolume.c
+++ UnidadedeVolume.c
@@ -1,2 +1,6 @@
 #include <stdio.h>
-
+void litrosParaOutros(double litros) {
+    printf("\n%.2f litros equivalem a:\n", litros);
+    printf("- %.2f mililitros\n", litros * 1000);
+    printf("- %.6f metros cúbicos\n", litros / 1000);
+}
