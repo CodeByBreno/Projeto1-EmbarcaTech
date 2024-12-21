d583b9fa32438ea3c4f043a5740ed59c70d62d87
Alisson Livio <livilia74629@gmail.com>
2024-12-20 23:53:40 -0300

    feat: adição das funções de conversão de Watts e Kilowatts para Cavalo-Vapor

--- ConversorPotencia.c
+++ ConversorPotencia.c
@@ -9,6 +9,8 @@ CONVERSOR DE UNIDADES DE POTÊNCIA
 void ConversorPotencia();
 void WattsKilowatts(float watts);
 void KillowattsWatts(float kilowatts);
+void WattsCv(float watts);
+void KilowattsCv(float kilowatts);
 
 int main(){
     
@@ -38,11 +40,25 @@ void ConversorPotencia(){
                 scanf("%f", &valor);
                 WattsKilowatts(valor);
                 break;
+            
             case 2:
                 printf("\nInforme o valor em Kilowatss: ");
                 scanf("%f", &valor);
                 KillowattsWatts(valor);
                 break;
+            
+            case 3: 
+                printf("\nInforme o valor em Watts: ");
+                scanf("%f", &valor);
+                WattsCv(valor);
+                break;
+            
+            case 4: 
+                printf("\nInforme o valor em Kilowatts: ");
+                scanf("%f", &valor);
+                KilowattsCv(valor);
+                break;
+
             default:
                 printf("Entrada Invalida!. Por favor escolha uma das Conversoes Indicadas!\n");
                 break;
@@ -55,10 +71,21 @@ void ConversorPotencia(){
 
 void WattsKilowatts(float watts){
     float conversao = watts / 1000;
-    printf("%.2f Watts (W) equivale a %.2f Quilowatts (kW)\n", watts, conversao);
+    printf("%.2f Watts (W) equivale a %.3f Quilowatts (kW)\n", watts, conversao);
 }
 
 void KillowattsWatts(float kilowatts){
     float conversao = kilowatts * 1000;
-     printf("%.2f Quilowatts (Kw) equivale a %.2f Watss (W)\n", kilowatts, conversao);
+    printf("%.2f Quilowatts (Kw) equivale a %.3f Watss (W)\n", kilowatts, conversao);
+}
+
+void WattsCv(float watts){
+    float conversao = watts / 735.5;
+    printf("%.2f Watts (W) equivale a %.3f Cavalo-Vapor (Cv)\n", watts, conversao);
+}
+
+void KilowattsCv(float kilowatts){
+    float conversao =  (kilowatts * 1000) / 735.5;
+    printf("%.2f Kilowatts (Kw) equivale a %.3f Cavalo-Vapor (Cv)\n", kilowatts, conversao);
+
 }
