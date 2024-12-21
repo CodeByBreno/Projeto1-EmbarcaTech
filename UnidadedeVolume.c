b0c93f95e732ef74cadaebfedd0d5994b8235f63
Lucas Cotrim <quinhacotrim@gmail.com>
2024-12-21 14:38:12 -0300

    Adcionando menu de interação e estrutura de repetição do programa

--- UnidadedeVolume.c
+++ UnidadedeVolume.c
@@ -14,3 +14,49 @@ void metrosCubicosParaOutros(double metrosCubicos) {
     printf("- %.2f litros\n", metrosCubicos * 1000);
     printf("- %.2f mililitros\n", metrosCubicos * 1000000);
 }
+int main() {
+    int opcao;
+    double valor;
+
+    printf("Conversor de Valores de Volume\n");
+    printf("Escolha uma opcao:\n");
+    printf("1 - Litros para Mililitros e Metros Cúbicos\n");
+    printf("2 - Mililitros para Litros e Metros Cúbicos\n");
+    printf("3 - Metros Cúbicos para Litros e Mililitros\n");
+    printf("0 - Sair\n");
+
+    while (1) {
+        printf("\nDigite sua opcao: ");
+        scanf("%d", &opcao);
+
+        if (opcao == 0) {
+            printf("Encerrando o programa...\n");
+            break;
+        }
+
+        switch (opcao) {
+            case 1:
+                printf("Digite o valor em litros: ");
+                scanf("%lf", &valor);
+                litrosParaOutros(valor);
+                break;
+
+            case 2:
+                printf("Digite o valor em mililitros: ");
+                scanf("%lf", &valor);
+                mililitrosParaOutros(valor);
+                break;
+
+            case 3:
+                printf("Digite o valor em metros cúbicos: ");
+                scanf("%lf", &valor);
+                metrosCubicosParaOutros(valor);
+                break;
+
+            default:
+                printf("Opcao invalida. Tente novamente.\n");
+        }
+    }
+
+    return 0;
+}
