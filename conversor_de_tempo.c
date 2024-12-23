5944857ad7f5d55d49f7d575e07769f02ad99a46
RivaldoMatias <88509897+RivaldoMatias@users.noreply.github.com>
2024-12-23 11:58:49 -0300

    Update conversor_de_tempo.c
    Adicionado menu com 4 opções de conversões e 1 ao final para continuar ou encerrar o programa

--- conversor_de_tempo.c
+++ conversor_de_tempo.c
@@ -19,23 +19,58 @@ int converterMinutosParaSegundos(int minutos) {
     return minutos * 60;
 }
 
+// Função para converter minutos em horas
+float converterMinutosParaHoras(int minutos) {
+    return minutos / 60.0;
+}
+
 int main() {
-    int segundos, horas, minutos;
+    int opcao, segundos, horas, minutos;
+    char continuar;
 
-    printf("Digite o tempo em segundos: ");
-    scanf("%d", &segundos);
-    converterTempo(segundos);
+    do {
+        printf("Selecione a opção de conversão:\n");
+        printf("1. Converter segundos para horas, minutos e segundos\n");
+        printf("2. Converter horas para minutos\n");
+        printf("3. Converter minutos para segundos\n");
+        printf("4. Converter minutos para horas\n");
+        printf("Digite a opção desejada: ");
+        scanf("%d", &opcao);
 
-    printf("Digite o tempo em horas: ");
-    scanf("%d", &horas);
-    minutos = converterHorasParaMinutos(horas);
-    printf("%d horas equivalem a %d minutos.\n", horas, minutos);
+        switch(opcao) {
+            case 1:
+                printf("Digite o tempo em segundos: ");
+                scanf("%d", &segundos);
+                converterTempo(segundos);
+                break;
+            case 2:
+                printf("Digite o tempo em horas: ");
+                scanf("%d", &horas);
+                minutos = converterHorasParaMinutos(horas);
+                printf("%d horas equivalem a %d minutos.\n", horas, minutos);
+                break;
+            case 3:
+                printf("Digite o tempo em minutos: ");
+                scanf("%d", &minutos);
+                segundos = converterMinutosParaSegundos(minutos);
+                printf("%d minutos equivalem a %d segundos.\n", minutos, segundos);
+                break;
+            case 4:
+                printf("Digite o tempo em minutos: ");
+                scanf("%d", &minutos);
+                float horasConvertidas = converterMinutosParaHoras(minutos);
+                printf("%d minutos equivalem a %.2f horas.\n", minutos, horasConvertidas);
+                break;
+            default:
+                printf("Opção inválida.\n");
+                break;
+        }
 
-    printf("Digite o tempo em minutos: ");
-    scanf("%d", &minutos);
-    segundos = converterMinutosParaSegundos(minutos);
-    printf("%d minutos equivalem a %d segundos.\n", minutos, segundos);
+        printf("Deseja fazer outra conversão? (s/n): ");
+        scanf(" %c", &continuar);
+    } while (continuar == 's' || continuar == 'S');
+
+    printf("Obrigado por usar nosso programa (By Rivaldo Matias.)\n");
 
     return 0;
 }
-
