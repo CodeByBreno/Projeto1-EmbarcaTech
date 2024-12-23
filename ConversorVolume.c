cf2c244bc9e770241f5498aef0b72ac328ccb00f
Breno Gabriel <bbbgab123@gmail.com>
2024-12-22 22:09:24 -0300

    Fix: Naming and excluding conflicting files

--- ConversorVolume.c
+++ ConversorVolume.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-void litrosParaOutros(double litros) {
+void litrosParaOutros(double litros)
+{
     printf("\n%.2f litros equivalem a:\n", litros);
     printf("- %.2f mililitros\n", litros * 1000);
     printf("- %.6f metros cúbicos\n", litros / 1000);
 }
-void mililitrosParaOutros(double mililitros) {
+void mililitrosParaOutros(double mililitros)
+{
     printf("\n%.2f mililitros equivalem a:\n", mililitros);
     printf("- %.2f litros\n", mililitros / 1000);
     printf("- %.9f metros cúbicos\n", mililitros / 1000000);
 }
-void metrosCubicosParaOutros(double metrosCubicos) {
+void metrosCubicosParaOutros(double metrosCubicos)
+{
     printf("\n%.6f metros cúbicos equivalem a:\n", metrosCubicos);
     printf("- %.2f litros\n", metrosCubicos * 1000);
     printf("- %.2f mililitros\n", metrosCubicos * 1000000);
 }
-int main() {
+int conversorVolume()
+{
     int opcao;
     double valor;
 
@@ -25,36 +29,39 @@ int main() {
     printf("3 - Metros Cúbicos para Litros e Mililitros\n");
     printf("0 - Sair\n");
 
-    while (1) {
+    while (1)
+    {
         printf("\nDigite sua opcao: ");
         scanf("%d", &opcao);
 
-        if (opcao == 0) {
+        if (opcao == 0)
+        {
             printf("Encerrando o programa...\n");
             break;
         }
 
-        switch (opcao) {
-            case 1:
-                printf("Digite o valor em litros: ");
-                scanf("%lf", &valor);
-                litrosParaOutros(valor);
-                break;
+        switch (opcao)
+        {
+        case 1:
+            printf("Digite o valor em litros: ");
+            scanf("%lf", &valor);
+            litrosParaOutros(valor);
+            break;
 
-            case 2:
-                printf("Digite o valor em mililitros: ");
-                scanf("%lf", &valor);
-                mililitrosParaOutros(valor);
-                break;
+        case 2:
+            printf("Digite o valor em mililitros: ");
+            scanf("%lf", &valor);
+            mililitrosParaOutros(valor);
+            break;
 
-            case 3:
-                printf("Digite o valor em metros cúbicos: ");
-                scanf("%lf", &valor);
-                metrosCubicosParaOutros(valor);
-                break;
+        case 3:
+            printf("Digite o valor em metros cúbicos: ");
+            scanf("%lf", &valor);
+            metrosCubicosParaOutros(valor);
+            break;
 
-            default:
-                printf("Opcao invalida. Tente novamente.\n");
+        default:
+            printf("Opcao invalida. Tente novamente.\n");
         }
     }
 
